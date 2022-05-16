e1a64ebf47ea67db63afd576e282d66966e4184d
Matheus Lindino <mclindino@inf.ufpel.edu.br>
2022-05-16 15:48:18 -0300

    Update run_app.sh

--- experiments/ativ-6-exp-1/run_app.sh
+++ experiments/ativ-6-exp-1/run_app.sh
@@ -8,6 +8,6 @@ PORT="1234"
 BATCH=16
 EPOCHS=1
 
-docker run -p $PORT:$PORT --env OMP_NUM_THREADS=1 --rm --user=$(id -u):$(id -g) --network=host -v=$(pwd):/root dist_dcgan:latest python -m torch.distributed.launch \
---nproc_per_node=$NPROC --nnodes=$NODES --node_rank=$RANK --master_addr=$IPMASTER --master_port=$PORT root/dist_dcgan.py --dataset cifar10 --dataroot ./cifar10 \
+docker run -p $PORT:$PORT --env OMP_NUM_THREADS=1 --rm --network=host -v=$(pwd):/root dist_dcgan:latest python -m torch.distributed.launch \
+--nproc_per_node=$NPROC --nnodes=$NODES --node_rank=$RANK --master_addr=$IPMASTER --master_port=$PORT root/dist_dcgan.py --dataset cifar10 --dataroot root/cifar10 \
 --batch_size=$BATCH --num_epochs=$EPOCHS
